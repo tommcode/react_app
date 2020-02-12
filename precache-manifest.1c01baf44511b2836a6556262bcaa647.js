b369572a948bfb42bf4eafa71e0f980e8a76704b
tomson95 <tomaszgr95@interia.pl>
2020-02-12 13:59:01 +0100

    Updates

---
+++ precache-manifest.1c01baf44511b2836a6556262bcaa647.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f76674a90552c8aa6d28",
+    "url": "/tommcode/react_app.git/static/js/runtime~main.cf9a3880.js"
+  },
+  {
+    "revision": "0c079193d3e2b51f7627",
+    "url": "/tommcode/react_app.git/static/js/main.f401c8f7.chunk.js"
+  },
+  {
+    "revision": "97b93e3a6531df7a2028",
+    "url": "/tommcode/react_app.git/static/js/2.b88a1623.chunk.js"
+  },
+  {
+    "revision": "0c079193d3e2b51f7627",
+    "url": "/tommcode/react_app.git/static/css/main.1cfad4a7.chunk.css"
+  },
+  {
+    "revision": "af4d5e88a3a7bc3e783c04047c3c1e0f",
+    "url": "/tommcode/react_app.git/index.html"
+  }
+];
